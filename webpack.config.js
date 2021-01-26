cd78ac00057600f024811c014391c8455fee1890
Akallabet <grazianos86@gmail.com>
2021-01-26 18:13:49 +0000

    use hot loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,21 @@
+const webpack = require('webpack')
 const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+// // const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.js',
+  entry: path.resolve(__dirname, './src/index.js'),
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: '[fullhash].app.js',
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js',
   },
   plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Chess Board',
-      template: 'src/index.html',
-    }),
+    // new CleanWebpackPlugin(),
+    // new HtmlWebpackPlugin({
+    //   title: 'Chess Board',
+    //   template: 'src/index.html',
+    // }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
     rules: [
@@ -27,20 +29,12 @@ module.exports = {
         exclude: /node_modules/,
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
-      {
-        test: /\.mp3$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: 'assets/sounds/[name].[ext]',
-          },
-        },
-      },
     ],
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    compress: true,
+    contentBase: path.join(__dirname, 'dist'),
+    // compress: true,
+    hot: true,
     port: 8000,
   },
 }

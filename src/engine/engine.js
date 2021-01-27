7eb7b1429d36cc3acf4617b60379952e7ea1ba71
Akallabet <grazianos86@gmail.com>
2021-01-27 09:36:54 +0000

    create random pawn and update FEN accordingly

--- src/engine/engine.js
+++ src/engine/engine.js
@@ -7,29 +7,75 @@ const pieces = {
   b: ['p', 'n', 'b', 'r', 'q', 'k'],
   w: ['P', 'N', 'B', 'R', 'Q', 'K'],
 }
+const getAvailableRows = (board) => {
+  const sumIfPiece = (total, cell) => (total + isNaN(cell) ? 1 : 0)
+  const isRowNotFull = ({ row }) => row.reduce(sumIfPiece, 0) < 8
+  const mapRowWithIndex = (row, rowIndex) => ({ rowIndex, row })
+
+  return board.map(mapRowWithIndex).filter(isRowNotFull)
+}
+
+const getRandomRow = (rows) => rows[Math.floor(Math.random() * rows.length)]
+const getRandomCell = (cells) => cells[Math.floor(Math.random() * cells.length)]
+
+const getFreeCells = ({ rowIndex, row }) => {
+  const isCellEmpty = ({ cell }) => !isNaN(cell)
+  const mapCellWithIndex = (cell, cellIndex) => ({ rowIndex, cellIndex, cell })
+  return row.map(mapCellWithIndex).filter(isCellEmpty)
+}
 
 export const engine = (args = defaultArgs) => {
-  const { fen } = args
+  let { fen } = args
+  const updateFEN = (newFEN) => (fen = newFEN)
 
-  const [
-    piecePlacement,
-    activeColor,
-    castlingAvailability,
-    enPassantTarget,
-    halfmoveClock,
-    fullmoveNumber,
-  ] = fen.split(' ')
+  const buildBoardFromFEN = () => {
+    const [piecePlacement] = fen.split(' ')
+    const addEmptyCells = (num) => [...Array(Number(num))].map(() => 0)
+    const buildCell = (row, cell) =>
+      isNaN(cell) ? [...row, cell] : [...row, ...addEmptyCells(cell)]
+    const buildRow = (rowPlacement) => rowPlacement.split('').reduce(buildCell, [])
 
-  const board = piecePlacement.split('/').map((rowPlacement) => {
-    return rowPlacement.split('').reduce((row, el) => {
-      if (isNaN(el)) return [...row, el]
-      return [...row, ...[...Array(Number(el))].map(() => 0)]
-    }, [])
-  })
-
-  return {
-    activeColor,
-    board,
-    fen,
+    return piecePlacement.split('/').map(buildRow)
   }
+
+  const buildFENFromBoard = (board) => {
+    const [_, ...rest] = fen.split(' ')
+    const buildRowPlacement = (row) =>
+      row.reduce((str, cell) => {
+        if (isNaN(cell)) return `${str}${cell}`
+        const lastInfo = str[str.length - 1]
+        if (isNaN(lastInfo)) return `${str}1`
+        return `${str}${lastInfo + 1}`
+      }, '')
+    return board.reduce((placement, row) => `${placement}/${buildRowPlacement(row)}`, '')
+  }
+
+  const addPieceToBoard = ({ board, piece, rowIndex, cellIndex }) => [
+    ...board.slice(0, rowIndex),
+    [...board[rowIndex].slice(0, cellIndex), piece, ...board[rowIndex].slice(cellIndex + 1)],
+    ...board.slice(rowIndex + 1),
+  ]
+
+  let board = buildBoardFromFEN()
+  const updateBoard = (newBoard) => (board = newBoard)
+
+  const getInfo = () => {
+    return {
+      activeColor: fen.split(' ')[1],
+      board,
+      fen,
+    }
+  }
+
+  const createRandomPawn = (color = colors[0]) => {
+    const piece = pieces[color][0]
+    const { rowIndex, cellIndex } = getRandomCell(
+      getFreeCells(getRandomRow(getAvailableRows(board)))
+    )
+    updateBoard(addPieceToBoard({ piece, board, rowIndex, cellIndex }))
+    updateFEN(buildFENFromBoard(board))
+    return getInfo()
+  }
+
+  return { getInfo, createRandomPawn }
 }
--- src/engine/engine.test.js
+++ src/engine/engine.test.js
@@ -3,7 +3,8 @@ import { engine } from './engine'
 const emptyBoard = [...Array(8)].map(() => [...Array(8)].map(() => 0))
 
 test('it should return an empty board', () => {
-  expect(engine()).toEqual({
+  const { getInfo } = engine()
+  expect(getInfo()).toEqual({
     activeColor: 'w',
     board: emptyBoard,
     fen: '8/8/8/8/8/8/8/8 w KQkq - 0 1',
@@ -11,9 +12,31 @@ test('it should return an empty board', () => {
 })
 
 test('it should return a board with a black pawn in second row, third column', () => {
-  expect(engine({ fen: '8/2p5/8/8/8/8/8/8 w KQkq - 0 1' })).toEqual({
+  const { getInfo } = engine({ fen: '8/2p5/8/8/8/8/8/8 w KQkq - 0 1' })
+  expect(getInfo()).toEqual({
     activeColor: 'w',
     board: [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 'p', 0, 0, 0, 0, 0], ...emptyBoard.slice(2, 8)],
     fen: '8/2p5/8/8/8/8/8/8 w KQkq - 0 1',
   })
+})
+
+test('it should return a board with two black pawn in second row', () => {
+  const { getInfo } = engine({ fen: '8/2p1p3/8/8/8/8/8/8 w KQkq - 0 1' })
+  expect(getInfo()).toEqual({
+    activeColor: 'w',
+    board: [[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 'p', 0, 'p', 0, 0, 0], ...emptyBoard.slice(2, 8)],
+    fen: '8/2p1p3/8/8/8/8/8/8 w KQkq - 0 1',
+  })
+})
+
+test('it should create a white pawn in a random place', () => {
+  const initialFen = '8/2p1p3/8/8/8/8/8/8 w KQkq - 0 1'
+  const { createRandomPawn } = engine({ fen: initialFen })
+  const { board, fen } = createRandomPawn()
+  expect(board).not.toEqual([
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 'p', 0, 'p', 0, 0, 0],
+    ...emptyBoard.slice(2, 8),
+  ])
+  expect(fen.split(' ')[0]).not.toEqual(initialFen.split(' ')[0])
 })

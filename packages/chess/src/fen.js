91e5f7b6647a4160160af98bd192e6fc63fa7b3a
Akallabet <grazianos86@gmail.com>
2022-12-28 11:50:54 +0100

    translate FEN string to object

--- packages/chess/src/fen.js
+++ packages/chess/src/fen.js
@@ -1,14 +1,36 @@
 import * as R from 'ramda';
 
+const emptyCell = '';
+const addEmptyCells = n => {
+  const cells = [];
+  for (let i = 0; i < Number(n); i++) {
+    cells.push(emptyCell);
+  }
+  return cells;
+};
+export const rowFromFEN = R.pipe(
+  R.split(''),
+  R.map(R.ifElse(isNaN, R.identity, addEmptyCells)),
+  R.flatten
+);
+
+const boardFromFEN = R.pipe(R.split('/'), R.map(rowFromFEN));
+
 export const fromFEN = FEN => {
-  const [pieces, activeColor, castlingRights, enPassant, halfMoves] = R.split(
-    ' ',
-    FEN
-  );
+  const [
+    piecePlacement,
+    activeColor,
+    castlingRights,
+    enPassant,
+    halfMoves,
+    fullMoves,
+  ] = R.split(' ', FEN);
   return {
+    piecePlacement: boardFromFEN(piecePlacement),
     activeColor,
     castlingRights: castlingRights === '-' ? [] : R.split('', castlingRights),
     enPassant: enPassant === '-' ? false : enPassant,
     halfMoves: Number(halfMoves),
+    fullMoves: Number(fullMoves),
   };
 };
--- packages/chess/src/fen.test.js
+++ packages/chess/src/fen.test.js
@@ -1,13 +1,40 @@
 import t from 'tap';
-import { fromFEN } from './fen.js';
+import { fromFEN, rowFromFEN } from './fen.js';
 
-t.test('FEN should return initial positions', t => {
+const emptyRow = ['', '', '', '', '', '', '', ''];
+const emptyBoard = [
+  emptyRow,
+  emptyRow,
+  emptyRow,
+  emptyRow,
+  emptyRow,
+  emptyRow,
+  emptyRow,
+  emptyRow,
+];
+
+t.test('Return object from FEN string', t => {
   const FENObj = fromFEN('8/8/8/8/8/8/8/8 w KQkq - 0 1');
   t.same(FENObj, {
+    piecePlacement: emptyBoard,
     activeColor: 'w',
     castlingRights: ['K', 'Q', 'k', 'q'],
     enPassant: false,
     halfMoves: 0,
+    fullMoves: 1,
+  });
+  t.end();
+});
+
+t.test('Create board rows from piece placement', t => {
+  t.plan(2);
+  t.test('Empty row', t => {
+    t.same(rowFromFEN('8'), emptyRow);
+    t.end();
+  });
+  t.test('Row with two pawns', t => {
+    t.same(rowFromFEN('2pp4'), ['', '', 'p', 'p', '', '', '', '']);
+    t.end();
   });
   t.end();
 });

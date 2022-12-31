5cf4407031c64cf9f7bfbbfc57af46c859023ce8
Akallabet <grazianos86@gmail.com>
2022-12-31 14:32:52 +0100

    use svg icon for pieces (start with bishop)

--- packages/chessboard/app/components/chessboard/index.tsx
+++ packages/chessboard/app/components/chessboard/index.tsx
@@ -1,34 +1,34 @@
-import type { ReactElement } from 'react';
+import * as R from 'ramda';
+import Bishop from './bishop';
+
 interface Square {
   piece: string;
-}
-interface SquareProps {
-  children: string;
 }
 
 const isEven = (n: number) => n % 2 === 0;
 const isOdd = (n: number) => !isEven(n);
 
-const BlackSquare = ({ children }: SquareProps) => (
-  <div className="bg-secondary h-full">{children}</div>
-);
-const WhiteSquare = ({ children }: SquareProps) => (
-  <div className="bg-primary h-full">{children}</div>
-);
-const Square = ({ children }: { children: ReactElement }) => (
-  <div className="h-full w-1/8">{children}</div>
-);
+const isWhitePiece = R.test(/[rnbqkp]/);
+
 export const ChessBoard = ({ board }: { board: Square[][] }) => (
   <div className="border-1 border-black flex flex-col w-full h-full-w sm:w-452 sm:h-452">
     {board.map((row, i) => (
       <div key={i} className="flex flex-row w-full h-1/8">
         {row.map(({ piece }, j) => {
-          const isWhite = (isEven(i) && isEven(j)) || (isOdd(i) && isOdd(j));
-          const Component = isWhite ? WhiteSquare : BlackSquare;
+          const isWhiteSquare =
+            (isEven(i) && isEven(j)) || (isOdd(i) && isOdd(j));
+          const bg = isWhiteSquare ? 'bg-primary' : 'bg-secondary';
           return (
-            <Square key={j}>
-              <Component>{piece}</Component>
-            </Square>
+            <div key={j} className="h-full w-1/8">
+              <div className={`${bg} h-full`}>
+                {piece && (
+                  <Bishop
+                    fill={isWhitePiece(piece) ? '#ffffff' : '#000000'}
+                    stroke={isWhitePiece(piece) ? '#000000' : '#ffffff'}
+                  />
+                )}
+              </div>
+            </div>
           );
         })}
       </div>

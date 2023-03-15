9c12004ffb57788d850c9551fc18ad2e45027847
Akallabet <grazianos86@gmail.com>
2023-03-15 23:48:48 +0000

    fix clear command

--- packages/chess/src/clear-board.js
+++ packages/chess/src/clear-board.js
@@ -1,4 +1,5 @@
 import * as R from 'ramda';
+import { addMetaData } from './add-metadata.js';
 import { fromFEN } from './fen/index.js';
 
-export const clearBoard = R.pipe(R.prop('FEN'), fromFEN);
+export const clearBoard = R.pipe(R.prop('FEN'), fromFEN, addMetaData);

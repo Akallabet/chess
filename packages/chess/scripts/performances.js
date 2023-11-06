0774a0cbffc63942f4354be081c1af1383ad7809
Graziano Statello <grazianos86@gmail.com>
2023-11-06 10:39:53 +0100

    compare performances with ChessJs

--- packages/chess/scripts/performances.js
+++ packages/chess/scripts/performances.js
@@ -1,21 +1,62 @@
+import { Chess } from 'chess.js';
 import { move, start, startFromPGN } from '../dist/index.js';
 import { matches } from '../test/fixtures/pgn.js';
 
-function simpleOneMove() {
-  const begin = performance.now();
-  const FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
-  move('e4', start({ FEN, mode: 'standard' }));
-  const end = performance.now();
-  console.log(`Simple: ${end - begin}ms`);
-}
+const data = {
+  simple: {
+    start: { FEN: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1' },
+    moves: ['e4'],
+  },
+  gameOfTheCentury: {
+    start: { PGN: matches.gameOfTheCentury },
+  },
+};
 
-function gameOfTheCentury() {
-  const begin = performance.now();
-  startFromPGN({ PGN: matches.gameOfTheCentury });
+const myImplementation = {
+  withFEN(args) {
+    const begin = performance.now();
+    args.moves.reduce((state, moveData) => {
+      return move(moveData, state);
+    }, start(args.start));
+    const end = performance.now();
+    console.log(`Simple: ${end - begin}ms`);
+  },
+  withPGN(args) {
+    const begin = performance.now();
+    startFromPGN(args.start);
 
-  const end = performance.now();
-  console.log(`Match of the century: ${end - begin}ms`);
-}
+    const end = performance.now();
+    console.log(`Match of the century: ${end - begin}ms`);
+  },
+};
 
-simpleOneMove();
-gameOfTheCentury();
+const withChessJs = {
+  withFEN(args) {
+    const begin = performance.now();
+    const chess = new Chess(args.start.FEN);
+
+    args.moves.forEach(moveData => {
+      return chess.move(moveData);
+    });
+
+    const end = performance.now();
+    console.log(`Simple: ${end - begin}ms`);
+  },
+  withPGN(args) {
+    const begin = performance.now();
+    const chess = new Chess();
+
+    chess.loadPgn(args.start.PGN);
+
+    const end = performance.now();
+    console.log(`Match of the century: ${end - begin}ms`);
+  },
+};
+
+console.log('My implementation');
+myImplementation.withFEN(data.simple);
+myImplementation.withPGN(data.gameOfTheCentury);
+
+console.log('Chess.js');
+withChessJs.withFEN(data.simple);
+withChessJs.withPGN(data.gameOfTheCentury);
